6c466e43e9721cf6e87fb7d98731cf371154e4f0
Umasri Reddy <snrecpc@gmail.com>
2026-05-21 20:05:24 +0530

    Update project configuration and dataset path management

--- utils/config.py
+++ utils/config.py
@@ -1,17 +1,9 @@
 import os
-
-# ======================
-# ROOT PATH
-# ======================
 BASE_DIR = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "..")
 )
 
 DATASET_PATH = os.path.join(BASE_DIR, "dataset")
-
-# ======================
-# TRAINING SETTINGS
-# ======================
 BATCH_SIZE = 16
 EPOCHS = 5
 LR_SPEECH = 1e-3
@@ -20,17 +12,11 @@ LR_FUSION = 1e-4
 
 NUM_CLASSES = 7
 
-# ======================
-# AUDIO SETTINGS
-# ======================
 SAMPLE_RATE = 16000
 N_MFCC = 40
 N_MELS = 128
 MAX_LEN = 100
 
-# ======================
-# MODEL PATHS
-# ======================
 RESULTS_DIR = os.path.join(BASE_DIR, "Results")
 
 SPEECH_MODEL_PATH = os.path.join(RESULTS_DIR, "speech", "speech_best_model.pth")
